e0c94867539d972008c9d0f692ad2bf1e8b22de1
Link4Electronics <link4electronics@gmail.com>
2026-02-28 23:30:25 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,21 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm libdecor
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+if [ "${DEVEL_RELEASE-}" = "1" ]; then
+    package="augustus-git"
+    make-aur-package "$package"
+else
+    package="augustus"
+    sudo pacman -S --noconfirm "$package"
+fi
+
+pacman -Q "$package" | awk '{print $2; exit}' > ~/version
 
 # If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
